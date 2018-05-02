2b295d86f7ba2986b912ce1905a0c4aa4057217d
pchaosgit <drifthua@gmail.com>
2018-05-02 19:24:10 +0800

    change tests directory add setup.py

--- ONEILQUANT/OneilQUANT/tests/test_oneilKDZD.py
+++ ONEILQUANT/OneilQUANT/tests/test_oneilKDZD.py
@@ -7,7 +7,6 @@
 @Contact : p19992003#gmail.com
 """
 from unittest import TestCase
-from ONEILQUANT.TESTS import env
 # append parent directory to import path
 from ONEILQUANT.OneilQUANT.Oneil import OneilKDZD as oneil
 
@@ -24,11 +23,11 @@ class TestOneilKDZD(TestCase):
     n1=365
     df = self.oq.listingDate(n1)
     # 上市超过一年的股票数量大于2000个
-    self.assertTrue(len(df) > 2000, "上市一年以上股票数量，2018年最少大于2000只")
+    self.assertTrue(len(df) > 2500, "上市一年以上股票数量，2018年最少大于2500只")
     # 不同上市时间的股票数比较
     n2 = 200
     df1 = self.oq.listingDate(n2)
-    self.assertTrue(len(df) < len(df1), "上市一年的股票数量<200天的股票数量")
+    self.assertTrue(len(df) < len(df1), "上市一年的股票数量<{0}天的股票数量".format(n2))
 
 # if __name__=='__main__':
 #   TestCase.run()
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='stock-by-William-O-Neil',
+    version='0.1',
+    packages=['ONEILQUANT', 'ONEILQUANT.BASE', 'ONEILQUANT.OneilQUANT', 'ONEILQUANT.OneilQUANT.tests',
+              'ONEILQUANT.OneilQUANT.tests.BASE'],
+    url='',
+    license='GPL V3',
+    author='chaos',
+    author_email='p19992003@gmail.com',
+    description='陶博士相关库'
+)
